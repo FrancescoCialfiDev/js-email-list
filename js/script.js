e882a859c82370286b39480b7a47b87921f00bc8
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2024-11-18 18:26:41 +0100

    security control for spam button

--- js/script.js
+++ js/script.js
@@ -9,34 +9,35 @@ console.log(bottone);
 
 
 
-bottone.addEventListener("click", generateEmail)
+bottone.addEventListener("click", generateEmail);
+
 function generateEmail() {
     lista.innerHTML = "";
     let ind = 0;
-    const intervallo = setInterval(() => {
+    bottone.disabled = true;
 
+    const intervallo = setInterval(() => {
         ind++;
         axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
             .then(res => {
-                let lista = document.getElementById("lista")
+                let lista = document.getElementById("lista");
                 let arrayEmail = [];
 
                 arrayEmail.push(res.data.response);
                 console.log(res.data.response);
                 for (let i = 0; i < arrayEmail.length; i++) {
-                    lista.innerHTML += `<li>${arrayEmail[i]}</li>`
+                    lista.innerHTML += `<li>${arrayEmail[i]}</li>`;
                 }
-            })
+            });
+
         if (ind === 10) {
-            clearInterval(intervallo)
+            clearInterval(intervallo);
+            bottone.disabled = false;
         }
-
-
     }, 600);
-
-
-
 }
+
+
 
 
 
